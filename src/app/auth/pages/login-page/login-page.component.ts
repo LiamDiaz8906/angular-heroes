23ad670a65a917aa8c718f2d240ae4843395e1cf
Liam Díaz <liamdiaz59@gmail.com>
2025-01-14 16:59:54 -0600

    Fin de modulo 16

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class LoginPageComponent {
+
+  constructor(
+    private autService: AuthService,
+    private router: Router
+  ) {}
+
+  onLogin(): void {
+
+    this.autService.login('liamdiaz50.com', '123456')
+      .subscribe( user => {
+        this.router.navigate(['/'])
+      } )
+
+  }
 
 }
